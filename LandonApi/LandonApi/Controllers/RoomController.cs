caf6309c64a7dc0b56f8d0101a7dafbd425f542a
shukla18 <shantanu-shukla@hotmail.com>
2024-07-30 14:40:58 +0530

    LandonApi initial draft

---
+++ LandonApi/LandonApi/Controllers/RoomController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LandonApi.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+
+        [HttpGet(Name = nameof(GetRooms))]
+        public ActionResult GetRooms()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LandonApi/LandonApi/Controllers/RootController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LandonApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRoot))]
+        public IActionResult GetRoot()
+        {
+            var response = new
+            {
+                // ion compatiable json
+                href = Url.Link(nameof(GetRoot), null),
+
+                rooms = new
+                {
+                    href = Url.Link(nameof(RoomController.GetRooms), null)
+                }
+            };
+
+            return Ok(response);
+        }
+    }
+}
